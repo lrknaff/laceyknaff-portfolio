c903eb70d937cdc4c8c5324f88d8a34b433d8144
lrknaff <lacey.knaff@gmail.com>
2016-11-18 19:34:01 -0700

    tinkering with tweening

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { TweenMax } from 'gsap'
 
 export default class Application extends Component {
+  componentDidMount() {
+    const node = this.node
+    TweenMax.to(node, 5, { x: 299 })
+  }
   render() {
     return (
-      <div>
+      <div ref={(c) => { this.node = c }}>
         Hello!
       </div>
     )
