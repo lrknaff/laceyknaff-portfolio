564354746590314f04229861d39ba8fd6fde51ff
lrknaff <lacey.knaff@gmail.com>
2016-11-18 23:46:52 -0700

    add layout styling to left container

--- lib/components/SplitLeftMain.jsx
+++ lib/components/SplitLeftMain.jsx
@@ -4,31 +4,33 @@ export default class SplitLeftMain extends Component {
   render() {
     return (
       <section className="split-item split-left-container">
-        <h1 className="split-left-text fade-in-up">Hi, I'm Lacey.</h1>
-        <h3 className="split-left-text fade-in-up">Graphic Designer and Front-End Developer.</h3>
-        <button className="button fade-in-up">say hello</button>
-        <div className="split-left-social">
-          <a href="https://twitter.com/laceyknaff">
-            <img
-              src="../lib/images/twitter.svg"
-              className="social-icon"
-              alt="twitter"
-            />
-          </a>
-          <a href="https://github.com/lrknaff">
-            <img
-              src="../lib/images/gitHub.svg"
-              className="social-icon"
-              alt="git hub"
-            />
-          </a>
-          <a href="https://www.linkedin.com/in/lacey-knaff-7a503233">
-            <img
-              src="../lib/images/linkedin.svg"
-              className="social-icon"
-              alt="linkedin"
-            />
-          </a>
+        <div className="split-left-text-block">
+          <h1 className="split-left-text fade-in-up">Hi, I'm Lacey.</h1>
+          <h3 className="split-left-text fade-in-up">Graphic Designer and Front-End Developer.</h3>
+          <button className="button fade-in-up">say hello</button>
+          <div className="split-left-social">
+            <a href="https://twitter.com/laceyknaff">
+              <img
+                src="../lib/images/twitter.svg"
+                className="social-icon"
+                alt="twitter"
+              />
+            </a>
+            <a href="https://github.com/lrknaff">
+              <img
+                src="../lib/images/gitHub.svg"
+                className="social-icon"
+                alt="git hub"
+              />
+            </a>
+            <a href="https://www.linkedin.com/in/lacey-knaff-7a503233">
+              <img
+                src="../lib/images/linkedin.svg"
+                className="social-icon"
+                alt="linkedin"
+              />
+            </a>
+          </div>
         </div>
       </section>
     )
