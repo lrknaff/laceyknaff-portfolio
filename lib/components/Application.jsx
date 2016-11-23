31d1e4462f20c3b6de1a895c0b907085063803c7
lrknaff <lacey.knaff@gmail.com>
2016-11-23 15:46:44 -0700

    add gsap stagger styling on left text block

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -7,16 +7,11 @@ import SplitLeftMain from './SplitLeftMain'
 import SplitRightAbout from './SplitRightAbout'
 
 export default class Application extends Component {
-  // componentDidMount() {
-  //   const node = this.node
-  //   TweenMax.to(node, 5, { x: 299 })
-  // }
   render() {
     return (
       <article className="split-container">
         <SplitLeftMain />
         <SplitRightAbout />
-        {/* <h1 ref={(c) => { this.node = c }}>Hello!</h1> */}
       </article>
     )
   }
--- lib/components/SplitLeftMain.jsx
+++ lib/components/SplitLeftMain.jsx
@@ -1,18 +1,41 @@
 import React, { Component } from 'react'
-// import ScrollEffect from 'react-scroll-effects'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import { TweenMax, staggerFrom, Power2 } from 'gsap'
 
 export default class SplitLeftMain extends Component {
+  componentDidMount() {
+    // const node = this.node
+    const e1 = this.one
+    const e2 = this.two
+    const e3 = this.three
+    const e4 = this.four
+    // TweenMax.to(node, 1, { opacity: 0, y: -5 })
+    TweenMax.staggerFrom([e1, e2, e3, e4], 1, { opacity: 0, scale: 0.5, y: 50, delay: 0.5, ease: Power2.easeIn }, 0.2)
+  }
   render() {
     return (
       <section className="split-item split-left-container">
         <div className="split-left-text-block">
-          {/* <ScrollEffect animate="fadeInUp">
-            <h1>test 1</h1>
-          </ScrollEffect> */}
-          <h1 className="split-left-text animated fadeInUp">Hi, I'm Lacey.</h1>
-          <h3 className="split-left-text animated fadeInUp">Graphic Designer and Front-End Developer.</h3>
-          <button className="button animated fadeInUp">say hello</button>
-          <div className="split-left-social animated fadeInUp">
+          <h1
+            className="split-left-text"
+            ref={(c) => { this.one = c }}
+          >Hi, I'm Lacey.
+          </h1>
+          <h3
+            className="split-left-text"
+            ref={(c) => { this.two = c }}
+          >Graphic Designer and Front-End Developer.
+          </h3>
+          <div ref={(c) => { this.three = c }}>
+            <button
+              className="button"
+            >say hello
+            </button>
+          </div>
+          <div
+            className="split-left-social"
+            ref={(c) => { this.four = c }}
+          >
             <a href="https://twitter.com/laceyknaff">
               <img
                 src="../lib/images/twitter.svg"
