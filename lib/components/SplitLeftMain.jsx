5732f2d925a75f8efb8aff7547890f922d6c225c
lrknaff <lacey.knaff@gmail.com>
2016-11-22 17:58:12 -0700

    add css animation.css dependency

--- lib/components/SplitLeftMain.jsx
+++ lib/components/SplitLeftMain.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+// import ScrollEffect from 'react-scroll-effects'
 
 export default class SplitLeftMain extends Component {
   render() {
     return (
       <section className="split-item split-left-container">
         <div className="split-left-text-block">
-          <h1 className="split-left-text fade-in-up">Hi, I'm Lacey.</h1>
-          <h3 className="split-left-text fade-in-up">Graphic Designer and Front-End Developer.</h3>
-          <button className="button fade-in-up">say hello</button>
-          <div className="split-left-social">
+          {/* <ScrollEffect animate="fadeInUp">
+            <h1>test 1</h1>
+          </ScrollEffect> */}
+          <h1 className="split-left-text animated fadeInUp">Hi, I'm Lacey.</h1>
+          <h3 className="split-left-text animated fadeInUp">Graphic Designer and Front-End Developer.</h3>
+          <button className="button animated fadeInUp">say hello</button>
+          <div className="split-left-social animated fadeInUp">
             <a href="https://twitter.com/laceyknaff">
               <img
                 src="../lib/images/twitter.svg"
