c4aaef1b64cad52b4f242b2b134b3cf2023aea94
lrknaff <lacey.knaff@gmail.com>
2016-11-24 13:24:15 -0700

    add work experience

--- lib/components/SplitRightAbout.jsx
+++ lib/components/SplitRightAbout.jsx
@@ -28,23 +28,22 @@ export default class SplitRightAbout extends Component {
     return (
       <section className="split-item split-right-container">
         <HamburgerNav />
-        <div className="split-right-faded-border" />
         <section
-          id="about-me"
-          className="section-spacing"
+          className="split-right-about-me section-spacing"
         >
           <h4 ref={(c) => { this.one = c }}>About</h4>
-          <p ref={(c) => { this.two = c }}>Lacey is a designer and front-end developer based in Denver, Colorado. She's in love with everything design and development, and has a passion for turning complex ideas into simple, purpose driven, solutions. After dabbling in far to many design related fields she finally found her passion for developing and designing on the web and is known for her clean and minimal design aesthetic. Her other passions include working on her vintage motorcycle, drinking beer on patios, riding her bike down mountains, shredding the gnar, and being a kind human being. She also thinks her dog might be cooler than she is.<span><br />These are some skills and programs she uses along the way:</span></p>
+          <p ref={(c) => { this.two = c }}>Lacey is a designer and front-end developer with a passion for turning complex ideas into simple, purpose driven, solutions. After dabbling in far too many design related fields she finally found her passion for developing and designing on the web and is known for her clean and minimal design aesthetic.<span><br />These are some skills and programs she uses along the way:</span></p>
           <ul className="skills" ref={(c) => { this.three = c }}>
-            <li><span>Adobe Illustrator/InDesign/Photoshop/XD</span></li>
+            <li><span>Adobe Creative Suite</span></li>
             <li><span>Sketch</span></li>
-            <li><span>HTML/CSS</span></li>
+            <li><span>HTML/CSS/Sass</span></li>
             <li><span>JavaScript/JQuery</span></li>
+            <li><span>React</span></li>
             <li><span>GitHub</span></li>
           </ul>
         </section>
 
-        <section className="experience section-spacing">
+        <section className="split-right-experience section-spacing">
           <h4>Experience</h4>
 
           <div className="work-experience">
@@ -57,7 +56,7 @@ export default class SplitRightAbout extends Component {
             </div>
           </div>
 
-          <div className="work-experience section-spacing">
+          <div className="work-experience">
             <div className="work-description">
               <h5>Digital Designer</h5>
               <p>Crocs</p>
