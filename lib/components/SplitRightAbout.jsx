cc60979f0932618ec0b67de3d8ae08fb309ee3c7
lrknaff <lacey.knaff@gmail.com>
2016-11-24 16:31:05 -0800

    merging navigation to master

--- lib/components/SplitRightAbout.jsx
+++ lib/components/SplitRightAbout.jsx
@@ -27,7 +27,7 @@ export default class SplitRightAbout extends Component {
       {
         opacity: 0,
         y: 50,
-        delay: 0.12,
+        delay: 0.2,
         ease: Power2.easeOut },
       0.12)
   }
